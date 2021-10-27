4d98d66c24383992b1f848342bda4fc9ab8dd07c
Valeriya <vvsergeeva@inbox.ru>
2021-10-27 23:09:13 +0300

    Added request hook

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, {useState} from 'react';
-import useDebounce from "./hooks/useDebounce";
+import React from 'react';
+import useRequest from "./hooks/useRequest";
+import axios from "axios";
 
 function App() {
-    const [value, setValue] = useState('');
-    const debouncedSearch = useDebounce(search, 500)
+    const [todos, loading, error] = useRequest(fetchTodos)
 
-    function search(query) {
-        fetch(`https://jsonplaceholder.typicode.com/todos?query=`+query)
-            .then(response => response.json())
-            .then(json => {
-                console.log(json)
-            })
+    function fetchTodos(query) {
+            return axios.get(`https://jsonplaceholder.typicode.com/todos?query=`+query)
     }
 
-    const onChange = e => {
-       setValue(e.target.value);
-       debouncedSearch(e.target.value);
+    if (loading) {
+        return <h1>Is loading..</h1>
+    }
+    if (error) {
+        return <h1>Error</h1>
     }
 
-  return (
-    <div>
-        <input type="text" value={value} onChange={onChange} />
-
-    </div>
-  );
+    return (
+        <div>
+            {todos && todos.map(todo =>
+                <div key={todo.id} style={{padding: '30px', border: '1px solid #000'}}>
+                    {todo.id}. {todo.title}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
